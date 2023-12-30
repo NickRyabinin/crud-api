35d7aa44084c0652762a44437ff3fe7b4b3aaea4
Николай <rnn2002@mail.ru>
2023-12-30 16:59:59 +0300

    Add Database connection class

---
+++ www/src/Database.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace app;
+
+final class Database
+{
+    private static ?Database $connection = null;
+
+    protected function __construct()
+    {
+    }
+
+    public function connect()
+    {
+        $databaseUrl = parse_url((string) getenv('DATABASE_URL'));
+        $username = $databaseUrl['user'];
+        $password = $databaseUrl['pass'];
+        $host = $databaseUrl['host'];
+        $port = $databaseUrl['port'];
+        $dbName = ltrim($databaseUrl['path'], '/');
+        $dsn = "mysql:host={$host};port={$port};dbname={$dbName}";
+        $options = [
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
+          ];
+        try {
+            $pdo = new \PDO($dsn, $username, $password, $options);
+            $migration = file_get_contents(__DIR__ . "/../migration.sql");
+            $statements = explode("\r\n\r\n", $migration);
+            foreach ($statements as $statement) {
+                $pdo->exec($statement);
+            }
+        } catch (\PDOException $e) {
+            echo $e->getMessage();
+            die();
+        }
+        return $pdo;
+    }
+
+    public static function get()
+    {
+        if (static::$connection === null) {
+            static::$connection = new self();
+        }
+
+        return static::$connection;
+    }
+}
