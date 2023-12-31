a06d9605b3c2cac8204f91530a8ca0f93ba4b675
Николай <rnn2002@mail.ru>
2023-12-31 18:45:04 +0300

    Modify index

--- www/index.php
+++ www/index.php
@@ -1,9 +1,15 @@
 <?php
 
+namespace web;
+
 require_once __DIR__ . '/templates/main.phtml';
 require_once __DIR__ . '/src/Database.php';
 
 use app\Database;
 
-$pdo = Database::get()->connect();
+const DB_TYPE = 'mysql';
+const MIGRATION_PATH = __DIR__ . "/migration.sql";
+
+$pdo = Database::get()->connect(DB_TYPE);
+Database::get()->migrate($pdo, MIGRATION_PATH);
 echo "I'm not running yet...";
