857c0173a6f26df4ffc8a2bbd12548d8ea053d10
Николай <rnn2002@mail.ru>
2023-12-31 18:44:25 +0300

    Modify src/Database - add "migrate()" method

--- www/src/Database.php
+++ www/src/Database.php
@@ -10,29 +10,37 @@ final class Database
     {
     }
 
-    public function connect()
+    public function connect(string $dbType)
     {
         $databaseUrl = parse_url((string) getenv('DATABASE_URL'));
-        $username = $databaseUrl['user'];
-        $password = $databaseUrl['pass'];
-        $host = $databaseUrl['host'];
-        $port = $databaseUrl['port'];
+        $username = $databaseUrl['user'] ?? '';
+        $password = $databaseUrl['pass'] ?? '';
+        $host = $databaseUrl['host'] ?? '';
+        $port = $databaseUrl['port'] ?? '';
         $dbName = ltrim($databaseUrl['path'], '/');
-        $dsn = "mysql:host={$host};port={$port};dbname={$dbName}";
+        $dsn = "{$dbType}:host={$host};port={$port};dbname={$dbName}";
         $options = [
             \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
             \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
           ];
         try {
             $pdo = new \PDO($dsn, $username, $password, $options);
-            $migration = file_get_contents(__DIR__ . "/../migration.sql");
+        } catch (\PDOException $e) {
+            die("Database connection failed: " . $e->getMessage());
+        }
+        return $pdo;
+    }
+
+    public function migrate(\PDO $pdo, string $migrationPath)
+    {
+        try {
+            $migration = file_get_contents($migrationPath);
             $statements = explode("\r\n\r\n", $migration);
             foreach ($statements as $statement) {
                 $pdo->exec($statement);
             }
         } catch (\PDOException $e) {
-            echo $e->getMessage();
-            die();
+            die("Database migration failed: " . $e->getMessage());
         }
         return $pdo;
     }
@@ -42,7 +50,6 @@ final class Database
         if (static::$connection === null) {
             static::$connection = new self();
         }
-
         return static::$connection;
     }
 }
