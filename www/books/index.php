c9094cc347f841e28276ac85fe59ac82fbf94496
Николай <rnn2002@mail.ru>
2024-01-04 19:19:47 +0300

    Modify books/index - add some input data checks

--- www/books/index.php
+++ www/books/index.php
@@ -18,55 +18,104 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 switch ($method) {
     case 'GET':
-    // Read
+        // Read
         $stmt = $pdo->query('SELECT * FROM books');
-        $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
-        echo json_encode($result);
-    break;
+        $booksCount = $pdo->query('SELECT COUNT(*) AS count FROM books')->fetch();
+        if ($stmt && $booksCount['count'] > 0) {
+            $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+            echo json_encode($result);
+        } else {
+            echo json_encode(['message' => 'No records found']);
+        }
+        break;
     case 'POST':
-    // Create
-        $data = json_decode(file_get_contents('php://input'), true);
-        $title = sanitize($data['title']);
-        $author = sanitize($data['author']);
-        $published_at = sanitize($data['published_at']);
+        // Create
+        $layout = ['title', 'author', 'published_at'];
+        $data = json_decode(file_get_contents('php://input'), true) ?? [];
+        if (compare($layout, $data)) {
+            $title = sanitize($data['title']);
+            $author = sanitize($data['author']);
+            $published_at = sanitize($data['published_at']);
 
-        $stmt = $pdo->prepare('INSERT INTO books (title, author, published_at) VALUES (?, ?, ?)');
-        $stmt->execute([$title, $author, $published_at]);
-
-        http_response_code(201);
-        echo json_encode(['message' => 'Book added successfully']);
-    break;
+            $stmt = $pdo->prepare('INSERT INTO books (title, author, published_at) VALUES (?, ?, ?)');
+            try {
+                if ($stmt->execute([$title, $author, $published_at])) {
+                    http_response_code(201);
+                    echo json_encode(['message' => 'Book added successfully']);
+                } else {
+                    sendError();
+                }
+            } catch (\PDOException $e) {
+                sendError();
+            }
+        } else {
+            sendError();
+        }
+        break;
     case 'PUT':
-    // Update
-        $data = json_decode(file_get_contents("php://input"), true);
-        $id = sanitize($data['id']);
-        $title = sanitize($data['title']);
-        $author = sanitize($data['author']);
-        $published_at = sanitize($data['published_at']);
+        // Update
+        $layout = ['id', 'title', 'author', 'published_at'];
+        $data = json_decode(file_get_contents("php://input"), true) ?? [];
+        if (compare($layout, $data)) {
+            $id = sanitize($data['id']);
+            $title = sanitize($data['title']);
+            $author = sanitize($data['author']);
+            $published_at = sanitize($data['published_at']);
 
-        $stmt = $pdo->prepare('UPDATE books SET title=?, author=?, published_at=? WHERE id=?');
-        $stmt->execute([$title, $author, $published_at, $id]);
-
-        echo json_encode(['message' => 'Book updated successfully']);
-    break;
+            $stmt = $pdo->prepare('UPDATE books SET title=?, author=?, published_at=? WHERE id=?');
+            try {
+                if ($stmt->execute([$title, $author, $published_at, $id])) {
+                    echo json_encode(['message' => 'Book updated successfully']);
+                } else {
+                    sendError();
+                }
+            } catch (\PDOException $e) {
+                sendError();
+            }
+        } else {
+            sendError();
+        }
+        break;
     case 'DELETE':
-    // Delete
-        $data = json_decode(file_get_contents("php://input"), true);
-        $id = sanitize($data['id']);
+        // Delete
+        $layout = ['id'];
+        $data = json_decode(file_get_contents("php://input"), true) ?? [];
+        if (compare($layout, $data)) {
+            $id = sanitize($data['id']);
 
-        $stmt = $pdo->prepare('DELETE FROM books WHERE id=?');
-        $stmt->execute([$id]);
-
-        echo json_encode(['message' => 'Book deleted successfully']);
-    break;
+            $stmt = $pdo->prepare('DELETE FROM books WHERE id=?');
+            try {
+                if ($stmt->execute([$id])) {
+                    echo json_encode(['message' => 'Book deleted successfully']);
+                } else {
+                    sendError();
+                }
+            } catch (\PDOException $e) {
+                sendError();
+            }
+        } else {
+            sendError();
+        }
+        break;
     default:
-     // Invalid method
+        // Invalid method
         http_response_code(405);
         echo json_encode(['error' => 'Method not allowed']);
-    break;
+        break;
 }
 
 function sanitize($param)
 {
     return htmlspecialchars(strip_tags($param));
+}
+
+function compare(array $layout, array $input): bool
+{
+    return (count($layout) == count($input) && array_diff($layout, array_keys($input)) == []);
+}
+
+function sendError()
+{
+    http_response_code(400);
+    echo json_encode(['error' => 'Invalid input JSON data']);
 }
