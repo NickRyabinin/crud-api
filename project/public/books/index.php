ae9872261b4c85890a069944c29eb1541f3a631a
Николай <rnn2002@mail.ru>
2024-01-16 18:27:31 +0300

    Modify public/books/index.php - some refactoring

--- project/public/books/index.php
+++ project/public/books/index.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../../App/Core/Container.php';
 require_once __DIR__ . '/../../App/Core/Database.php';
+require_once __DIR__ . '/../../App/Core/Helper.php';
 require_once __DIR__ . '/../../App/Routers/BookRouter.php';
 require_once __DIR__ . '/../../App/Models/Book.php';
 require_once __DIR__ . '/../../App/Controllers/BookController.php';
@@ -9,6 +10,7 @@ require_once __DIR__ . '/../../App/Views/BookView.php';
 
 use App\Core\Container;
 use App\Core\Database;
+use App\Core\Helper;
 use App\Routers\BookRouter;
 use App\Models\Book;
 use App\Controllers\BookController;
@@ -22,12 +24,14 @@ Database::get()->migrate($pdo, MIGRATION_PATH);
 
 $container = new Container();
 $container->set('pdo', $pdo);
+$container->set('helper', new Helper());
 $container->set('book', new Book($container->get('pdo')));
 $container->set('bookView', new BookView());
-$container->set('bookController', new BookController($container->get('book'), $container->get('bookView')));
-
-$method = $_SERVER['REQUEST_METHOD'];
-$request = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
+$container->set('bookController', new BookController(
+    $container->get('book'),
+    $container->get('bookView'),
+    $container->get('helper')
+));
 
 $router = new BookRouter($container);
-$router->route($method, $request);
+$router->route();
