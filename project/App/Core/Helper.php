de8b982b14dcd7557a0f01e6f20bf94c6024beb2
Николай <rnn2002@mail.ru>
2024-01-16 18:26:00 +0300

    Add "Helper" class with some useful methods

---
+++ project/App/Core/Helper.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Core;
+
+class Helper
+{
+    public function getHttpMethod(): string
+    {
+        return strtoupper($_SERVER['REQUEST_METHOD']);
+    }
+
+    public function getId(): string | bool
+    {
+        $request = $this->getRequest();
+        if (!empty($request[1])) {
+            $id = $this->sanitize($this->validate($request[1]));
+            return (is_numeric($id) && $id >= 0 && floor($id) == $id) ? $id : false;
+        }
+        return '';
+    }
+
+    public function getRequest(): array
+    {
+        return explode('/', trim($_SERVER['REQUEST_URI'], '/'));
+    }
+
+    public function getResource(): string
+    {
+        return $this->sanitize($this->validate($this->getRequest()[0]));
+    }
+
+    public function validate(mixed $data): string | bool
+    {
+        if (is_int($data) || is_string($data)) {
+            return $data;
+        }
+        return false;
+    }
+
+    public function sanitize(string $data): string
+    {
+        return htmlspecialchars(strip_tags($data));
+    }
+}
