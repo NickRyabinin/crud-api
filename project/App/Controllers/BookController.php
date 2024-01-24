ca74dc9ccb7f07642cb3b00ad7a0a4e91226af83
Николай <rnn2002@mail.ru>
2024-01-24 19:16:48 +0300

    Modify files - add token checks

--- project/App/Controllers/BookController.php
+++ project/App/Controllers/BookController.php
@@ -22,14 +22,21 @@ class BookController
     public function create()
     {
         $id = $this->helper->getId();
+        $token = $this->helper->getToken();
         $responseCode = '400';
         $message = ['error' => 'Invalid input data'];
         if ($id === '') {
             $inputData = $this->helper->getInputData();
             $cleanData = array_map(fn ($param) => $this->helper->sanitize($this->helper->validate($param)), $inputData);
-            if ($this->book->store($cleanData)) {
+            $message = $this->book->store($token, $cleanData);
+            if ($message === true) {
                 $responseCode = '201';
                 $message = ['message' => "Done, book added successfully"];
+            } elseif ($message === '') {
+                $responseCode = '401';
+                $message = ['error' => 'Unauthorized, no such token'];
+            } else {
+                $message = ['error' => 'Invalid input data'];
             }
         }
         $this->view->send($responseCode, $message);
@@ -64,19 +71,23 @@ class BookController
     public function update()
     {
         $id = $this->helper->getId();
+        $token = $this->helper->getToken();
         if ($id === '' || $id === false) {
             $responseCode = '400';
             $message = ['error' => 'Invalid ID'];
         } else {
             $inputData = $this->helper->getInputData();
             $cleanData = array_map(fn ($param) => $this->helper->sanitize($this->helper->validate($param)), $inputData);
-            $message = $this->book->update($id, $cleanData);
+            $message = $this->book->update($id, $token, $cleanData);
             if ($message === false) {
                 $responseCode = '404';
                 $message = ['error' => 'No record with such ID'];
             } elseif ($message === null) {
                 $responseCode = '400';
                 $message = ['error' => 'Invalid input data'];
+            } elseif ($message === '') {
+                $responseCode = '401';
+                $message = ['error' => 'Unauthorized, no such token'];
             } else {
                 $responseCode = '200';
                 $message = ["Done, book updated successfully"];
@@ -88,14 +99,18 @@ class BookController
     public function delete()
     {
         $id = $this->helper->getId();
+        $token = $this->helper->getToken();
         if ($id === '' || $id === false) {
             $responseCode = '400';
             $message = ['error' => 'Invalid ID'];
         } else {
-            $message = $this->book->destroy($id);
+            $message = $this->book->destroy($id, $token);
             if ($message === false) {
                 $responseCode = '404';
                 $message = ['error' => 'No record with such ID'];
+            } elseif ($message === '') {
+                $responseCode = '401';
+                $message = ['error' => 'Unauthorized, no such token'];
             } else {
                 $responseCode = '200';
                 $message = ["Done, book deleted successfully"];
@@ -111,21 +126,3 @@ class BookController
         $this->view->send($responseCode, $message);
     }
 }
-
-/* http_response_code(400);
-['error' => 'Invalid input data']
-['error' => 'Invalid ID']
-
-http_response_code(405);
-['error' => 'Method not allowed']
-
-http_response_code(404);
-['error' => 'No record with such ID']
-['error' => 'No records']
-
-http_response_code(201);
-['message' => "Done, {$entity} added successfully"]
-
-http_response_code(200);
-['message' => "Done, {$entity} updated successfully"]
-['message' => "Done, {$entity} deleted successfully"] */
--- project/App/Models/Book.php
+++ project/App/Models/Book.php
@@ -13,19 +13,25 @@ class Book
         $this->pdo = $pdo;
     }
 
-    public function store($data)
+    public function store($token, $data)
     {
+        $hashedToken = base64_decode($token);
         if ($this->compare($this->properties, $data)) {
-            try {
-                $query = "INSERT INTO {$this->entity}s (title, author, published_at)
-                    VALUES (:title, :author, :published_at)";
-                $stmt = $this->pdo->prepare($query);
-                foreach ($data as $key => $value) {
-                    $stmt->bindValue(":{$key}", $value);
+            if ($this->checkToken($hashedToken)) {
+                try {
+                    $query = "INSERT INTO {$this->entity}s (title, author, published_at)
+                        VALUES (:title, :author, :published_at)";
+                    $stmt = $this->pdo->prepare($query);
+                    foreach ($data as $key => $value) {
+                        $stmt->bindValue(":{$key}", $value);
+                    }
+                    $stmt->execute();
+                    return true;
+                } catch (\PDOException $e) {
+                    return false;
                 }
-                $stmt->execute();
-                return true;
-            } catch (\PDOException $e) {
+            } else {
+                return '';
             }
         }
         return false;
@@ -47,40 +53,49 @@ class Book
         return $stmt->fetch();
     }
 
-    public function update($id, $data)
+    public function update($id, $token, $data)
     {
+        $hashedToken = base64_decode($token);
         $filteredData = array_intersect_key($data, array_flip($this->properties));
         if ($this->checkId($id)) {
-            if (count($filteredData) > 0) {
-                $query = "UPDATE {$this->entity}s SET";
-                foreach ($filteredData as $key => $value) {
-                    $query = $query . " {$key} = :{$key},";
-                }
-                $query = substr($query, 0, -1) . " WHERE id = :id";
-                $stmt = $this->pdo->prepare($query);
-                $stmt->bindParam(":id", $id);
-                foreach ($filteredData as $key => $value) {
-                    $stmt->bindValue(":{$key}", $value);
-                }
-                try {
-                    if ($stmt->execute()) {
-                        return true;
+            if ($this->checkToken($hashedToken)) {
+                if (count($filteredData) > 0) {
+                    $query = "UPDATE {$this->entity}s SET";
+                    foreach ($filteredData as $key => $value) {
+                        $query = $query . " {$key} = :{$key},";
                     }
-                } catch (\PDOException $e) {
+                    $query = substr($query, 0, -1) . " WHERE id = :id";
+                    $stmt = $this->pdo->prepare($query);
+                    $stmt->bindParam(":id", $id);
+                    foreach ($filteredData as $key => $value) {
+                        $stmt->bindValue(":{$key}", $value);
+                    }
+                    try {
+                        if ($stmt->execute()) {
+                            return true;
+                        }
+                    } catch (\PDOException $e) {
+                        return null;
+                    }
                 }
+                return null;
             }
-            return null;
+            return '';
         }
         return false;
     }
 
-    public function destroy($id)
+    public function destroy($id, $token)
     {
+        $hashedToken = base64_decode($token);
         if ($this->checkId($id)) {
-            $query = "DELETE FROM {$this->entity}s WHERE id= :id";
-            $stmt = $this->pdo->prepare($query);
-            $stmt->execute([':id' => $id]);
-            return true;
+            if ($this->checkToken($hashedToken)) {
+                $query = "DELETE FROM {$this->entity}s WHERE id= :id";
+                $stmt = $this->pdo->prepare($query);
+                $stmt->execute([':id' => $id]);
+                return true;
+            }
+            return '';
         }
         return false;
     }
@@ -90,6 +105,14 @@ class Book
         $query = "SELECT EXISTS (SELECT id FROM {$this->entity}s WHERE id = :id) AS isExists";
         $stmt = $this->pdo->prepare($query);
         $stmt->execute([':id' => $id]);
+        return (($stmt->fetch())['isExists'] === 0) ? false : true;
+    }
+
+    private function checkToken($hashedToken)
+    {
+        $query = "SELECT EXISTS (SELECT id FROM users WHERE hashed_token = :hashed_token) AS isExists";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute([':hashed_token' => $hashedToken]);
         return (($stmt->fetch())['isExists'] === 0) ? false : true;
     }
 
