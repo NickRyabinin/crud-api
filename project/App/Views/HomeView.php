528eff1f2758a743ea74718480137e9fa3bccc4c
Николай <rnn2002@mail.ru>
2024-01-27 17:22:31 +0300

    Modify "home" page handlers

---
+++ project/App/Views/HomeView.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Views;
+
+class HomeView
+{
+    public function render(): void
+    {
+        header('Content-Type: text/html');
+        header('Access-Control-Allow-Origin: *');
+        http_response_code('200');
+        echo file_get_contents(__DIR__ . '/../../public/index.html');
+    }
+}
