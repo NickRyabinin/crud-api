c8717cc3c05f9ad935bb7d4b41d1f68f5a19dabb
Николай <rnn2002@mail.ru>
2024-02-12 14:20:02 +0300

    Modify Controller.php - divide complex methods to pieces

--- project/App/Controllers/Controller.php
+++ project/App/Controllers/Controller.php
@@ -33,37 +33,65 @@ abstract class Controller
     {
         $message = $this->model->index();
         if ($message === []) {
-            $responseCode = '404';
-            $message = ['error' => 'No records'];
-        } else {
-            $responseCode = '200';
+            $this->handleNoRecords();
         }
-        $this->view->send($responseCode, $message);
-    }
-
-    private function handleInvalidId()
-    {
-        $responseCode = '400';
-        $message = ['error' => 'Invalid ID'];
-        $this->view->send($responseCode, $message);
+        $this->handleOk($message);
     }
 
     private function handleValidId($id)
     {
         $message = $this->model->show($id);
         if ($message === false) {
-            $responseCode = '404';
-            $message = ['error' => 'No record with such ID'];
-        } else {
-            $responseCode = '200';
+            $this->handleNoRecord();
         }
+        $this->handleOk($message);
+    }
+
+    public function handleInvalidId()
+    {
+        $responseCode = '400';
+        $message = ['error' => 'Invalid ID'];
         $this->view->send($responseCode, $message);
     }
 
-    public function invalidMethod()
+    public function handleInvalidMethod()
     {
         $responseCode = '405';
         $message = ['error' => 'Method not allowed'];
+        $this->view->send($responseCode, $message);
+    }
+
+    public function handleInvalidData()
+    {
+        $responseCode = '400';
+        $message = ['error' => 'Invalid input data'];
+        $this->view->send($responseCode, $message);
+    }
+
+    public function handleInvalidToken()
+    {
+        $responseCode = '401';
+        $message = ['error' => 'Unauthorized, no such token'];
+        $this->view->send($responseCode, $message);
+    }
+
+    public function handleNoRecord()
+    {
+        $responseCode = '404';
+        $message = ['error' => 'No record with such ID'];
+        $this->view->send($responseCode, $message);
+    }
+
+    public function handleNoRecords()
+    {
+        $responseCode = '404';
+        $message = ['error' => 'No records'];
+        $this->view->send($responseCode, $message);
+    }
+
+    public function handleOk($message)
+    {
+        $responseCode = '200';
         $this->view->send($responseCode, $message);
     }
 }
