6e030bcca5e7df03a81bdd7d6b53d4fd70ccceb0
Николай <rnn2002@mail.ru>
2024-03-01 16:37:00 +0300

    Modify Opinion - fix bugs, add destroy() method

--- project/App/Models/Opinion.php
+++ project/App/Models/Opinion.php
@@ -7,21 +7,27 @@ use App\Core\Exceptions\InvalidDataException;
 class Opinion extends Model
 {
     public string $entity = 'opinion';
-    protected array $properties = ['opinion', 'book_id', 'author_login'];
+    protected array $properties = ['opinion', 'book_id', 'author_login', 'opinion_id'];
+    public array $belongsTo = ['book'];
     protected \PDO $pdo;
+    protected Book $book;
 
-    public function __construct(\PDO $pdo)
+    public function __construct(\PDO $pdo, Book $book)
     {
         $this->pdo = $pdo;
+        $this->book = $book;
     }
 
-    public function store(string $token, array $data): bool
+    public function store(string $parentId, string $token, array $data): bool
     {
         parent::checkToken($token);
+        $this->book->checkId($parentId);
+        $data['book_id'] = $parentId;
         $data['author_login'] = parent::get('user', 'login', 'token', $token);
+        $data['opinion_id'] = $this->getMaxChildId($parentId) + 1;
         parent::compare($this->properties, $data);
-        $query = "INSERT INTO {$this->entity}s (opinion, book_id, author_login)
-            VALUES (:opinion, :book_id, :author_login)";
+        $query = "INSERT INTO {$this->entity}s (opinion, book_id, author_login, opinion_id)
+            VALUES (:opinion, :book_id, :author_login, :opinion_id)";
         try {
             $stmt = $this->pdo->prepare($query);
             foreach ($data as $key => $value) {
@@ -48,9 +54,34 @@ class Opinion extends Model
 
     public function show(string $parentId, string $childId): array | bool
     {
-        $query = "SELECT * FROM {$this->entity}s WHERE book_id = :book_id AND id = :id";
+        $query = "SELECT * FROM {$this->entity}s WHERE book_id = :book_id AND opinion_id = :opinion_id";
         $stmt = $this->pdo->prepare($query);
-        $stmt->execute([':book_id' => $parentId, ':id' => $childId]);
+        $stmt->execute([':book_id' => $parentId, ':opinion_id' => $childId]);
         return $stmt->fetch();
+    }
+
+    public function destroy(string $parentId, string $childId, string $token): bool
+    {
+        parent::checkId($childId);
+        $this->book->checkId($parentId);
+        parent::checkToken($token);
+        $query = "DELETE FROM {$this->entity}s WHERE book_id = :book_id AND opinion_id = :opinion_id";
+        try {
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute([":book_id" => $parentId, ":opinion_id" => $childId]);
+        } catch (\PDOException $e) {
+            throw new InvalidDataException();
+        }
+        return true;
+    }
+
+    protected function getMaxChildId($parentId)
+    {
+        $stmt = $this->pdo->prepare(
+            "SELECT COALESCE(MAX(opinion_id), 0) as max_opinion_id FROM opinions WHERE book_id = :book_id"
+        );
+        $stmt->bindParam(':book_id', $parentId);
+        $stmt->execute();
+        return $stmt->fetch()['max_opinion_id'];
     }
 }
