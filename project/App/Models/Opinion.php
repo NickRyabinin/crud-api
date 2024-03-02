3441f22fef9ace2e63230161671b46531dbd1497
Николай <rnn2002@mail.ru>
2024-03-02 12:27:02 +0300

    Modify Opinion - add update() method

--- project/App/Models/Opinion.php
+++ project/App/Models/Opinion.php
@@ -8,6 +8,7 @@ class Opinion extends Model
 {
     public string $entity = 'opinion';
     protected array $properties = ['opinion', 'book_id', 'author_login', 'opinion_id'];
+    protected array $fillableProperties = ['opinion'];
     public array $belongsTo = ['book'];
     protected \PDO $pdo;
     protected Book $book;
@@ -22,10 +23,10 @@ class Opinion extends Model
     {
         parent::checkToken($token);
         $this->book->checkId($parentId);
+        parent::compare($this->fillableProperties, $data);
         $data['book_id'] = $parentId;
         $data['author_login'] = parent::get('user', 'login', 'token', $token);
         $data['opinion_id'] = $this->getMaxChildId($parentId) + 1;
-        parent::compare($this->properties, $data);
         $query = "INSERT INTO {$this->entity}s (opinion, book_id, author_login, opinion_id)
             VALUES (:opinion, :book_id, :author_login, :opinion_id)";
         try {
@@ -60,11 +61,40 @@ class Opinion extends Model
         return $stmt->fetch();
     }
 
+    public function update(string $parentId, string $childId, string $token, array $data): bool
+    {
+        parent::checkToken($token);
+        $this->book->checkId($parentId);
+        parent::checkId($childId);
+        $filteredData = array_intersect_key($data, array_flip($this->fillableProperties));
+        if (count($filteredData) === 0) {
+            throw new InvalidDataException();
+        }
+        $filteredData['author_login'] = parent::get('user', 'login', 'token', $token);
+        $query = "UPDATE {$this->entity}s SET";
+        foreach ($filteredData as $key => $value) {
+            $query = $query . " {$key} = :{$key},";
+        }
+        $query = rtrim($query, ',') . " WHERE book_id = :book_id AND opinion_id = :opinion_id";
+        try {
+            $stmt = $this->pdo->prepare($query);
+            foreach ($filteredData as $key => $value) {
+                $stmt->bindValue(":{$key}", $value);
+            }
+            $stmt->bindValue(":book_id", $parentId);
+            $stmt->bindValue(":opinion_id", $childId);
+            $stmt->execute();
+        } catch (\PDOException $e) {
+            throw new InvalidDataException();
+        }
+        return true;
+    }
+
     public function destroy(string $parentId, string $childId, string $token): bool
     {
-        parent::checkId($childId);
-        $this->book->checkId($parentId);
         parent::checkToken($token);
+        $this->book->checkId($parentId);
+        parent::checkId($childId);
         $query = "DELETE FROM {$this->entity}s WHERE book_id = :book_id AND opinion_id = :opinion_id";
         try {
             $stmt = $this->pdo->prepare($query);
@@ -75,7 +105,7 @@ class Opinion extends Model
         return true;
     }
 
-    protected function getMaxChildId($parentId)
+    protected function getMaxChildId(string $parentId): int
     {
         $stmt = $this->pdo->prepare(
             "SELECT COALESCE(MAX(opinion_id), 0) as max_opinion_id FROM opinions WHERE book_id = :book_id"
