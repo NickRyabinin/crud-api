02d31f92e3f9bd2b418ae5b3c4a58812ce3a547c
Николай <rnn2002@mail.ru>
2024-04-15 20:00:12 +0300

    Modify setenv.sh - change script for less user input

--- project/setenv.sh
+++ project/setenv.sh
@@ -2,24 +2,17 @@
 
 # Скрипт для установки переменных окружения для запуска docker-compose
 
-echo "Введите URL для подключения к базе данных (DATABASE_URL):"
+echo "Введите URL для подключения к базе данных (DATABASE_URL) вида 'pdoDBType://user:password@host:port/dbName' :"
 read DATABASE_URL
 
-echo "Введите пароль для root пользователя в MySQL (MYSQL_ROOT_PASSWORD):"
-read MYSQL_ROOT_PASSWORD
-
-echo "Введите название базы данных (MYSQL_DATABASE):"
-read MYSQL_DATABASE
-
-echo "Введите имя пользователя для доступа к базе данных (MYSQL_USER):"
-read MYSQL_USER
-
-echo "Введите пароль для пользователя доступа к базе данных (MYSQL_PASSWORD):"
-read MYSQL_PASSWORD
+# Разбираем переменную DATABASE_URL на составляющие
+MYSQL_USER=$(echo $DATABASE_URL | awk -F'[/:@]' '{print $4}')
+MYSQL_PASSWORD=$(echo $DATABASE_URL | awk -F'[/:@]' '{print $5}')
+MYSQL_DATABASE=$(echo $DATABASE_URL | awk -F'[/:@]' '{print $8}')
 
 # Установка переменных окружения
 export DATABASE_URL=$DATABASE_URL
-export MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD
+export MYSQL_ROOT_PASSWORD=$MYSQL_PASSWORD
 export MYSQL_DATABASE=$MYSQL_DATABASE
 export MYSQL_USER=$MYSQL_USER
 export MYSQL_PASSWORD=$MYSQL_PASSWORD
