1a967a31ec1ea3d9eb1d62d4e11df958fa2d015a
Николай <rnn2002@mail.ru>
2024-06-19 19:04:46 +0300

    Modify MessagesHandlers - add handleOptions() method

--- project/App/Controllers/MessagesHandlers.php
+++ project/App/Controllers/MessagesHandlers.php
@@ -115,4 +115,12 @@ trait MessagesHandlers
         $message = ['error' => 'Resource not found'];
         $this->view->send($responseCode, $message);
     }
+
+    protected function handleOptions(): void
+    {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: Authorization');
+        http_response_code(200);
+    }
 }
