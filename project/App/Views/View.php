ad600ced0a9ab273a44218020e7efdaded9098c9
Николай <rnn2002@mail.ru>
2024-06-19 21:04:16 +0300

    Modify View - trying to fix CORS problem with OPTIONS request

--- project/App/Views/View.php
+++ project/App/Views/View.php
@@ -10,10 +10,8 @@ class View
 {
     public function send(string $responseCode, array $message): void
     {
+        header('Access-Control-Allow-Origin: *');
         header('Content-Type: application/json');
-        /*header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
-        header('Access-Control-Allow-Headers: Authorization'); */
         http_response_code($responseCode);
         echo json_encode($message);
     }
