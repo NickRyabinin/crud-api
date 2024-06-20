fd6f5a00a6cf470d152f38c906268c1c62ad1b03
Николай <rnn2002@mail.ru>
2024-06-20 12:27:43 +0300

    Modify MessagesHandlers - trying to fix bug with CORS "no Access-Control-Allow-Origin"

--- project/App/Controllers/MessagesHandlers.php
+++ project/App/Controllers/MessagesHandlers.php
@@ -116,10 +116,10 @@ trait MessagesHandlers
         $this->view->send($responseCode, $message);
     }
 
-    protected function handleOptions(): void
+    public function handleOptions(): void
     {
-        // header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: POST');
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
         header('Access-Control-Allow-Headers: Authorization');
         header('Access-Control-Max-Age: 86400');
         header("Content-Length: 0");
